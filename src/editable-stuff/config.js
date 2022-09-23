a0ba8d6054a19514eba9eed7e23c1c40af451555
Sebastian Holguin <sebastian.holguin100@gmail.com>
2022-09-23 15:56:18 -0400

    Testing changes.

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -8,7 +8,7 @@ const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Sebastian",
   middleName: "",
-  lastName: "Holguin",
+  lastName: "Holguinaaa",
   message: " Welcome to my developer portfolio! ",
   icons: [
     {
